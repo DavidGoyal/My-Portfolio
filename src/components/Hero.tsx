631ae489c87cda14f68f4f1cb7899139b4b27287
David <goyaldavid55@gmail.com>
2024-10-27 15:42:13 +0530

    Did minor fixes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,7 +22,7 @@ const Hero = () => {
 						</button>
 						<button className="bg-blue-700 text-white rounded-md px-4 py-2 ">
 							<a
-								href="https://drive.google.com/file/d/1Al7Ug-lYjrXMaf3WyEi4BLA2yVHRrzel/view?usp=sharing"
+								href="https://drive.google.com/file/d/16s_s87Liznysd3uxU-3SHCx8gjGzeX8H/view?usp=sharing"
 								target="_blank"
 							>
 								Open Resume
